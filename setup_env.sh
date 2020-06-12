07c544a4d8f044d2eb7d8dd90773dbcb51de2507
gopicci <56777940+gopicci@users.noreply.github.com>
2020-06-11 18:34:24 -0700

    CI/CD

--- setup_env.sh
+++ setup_env.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-echo DEBUG=0 >> .env
-echo SQL_ENGINE=django.db.backends.postgresql >> .env
-echo DATABASE=postgres >> .env
+echo DEBUG=0 >> .env.prod
+echo SQL_ENGINE=django.db.backends.postgresql >> .env.prod
+echo DATABASE=postgres >> .env.prod
 
-echo SECRET_KEY=$SECRET_KEY >> .env
-echo SQL_DATABASE=$SQL_DATABASE >> .env
-echo SQL_USER=$SQL_USER >> .env
-echo SQL_PASSWORD=$SQL_PASSWORD >> .env
-echo SQL_HOST=$SQL_HOST >> .env
-echo SQL_PORT=$SQL_PORT >> .env
+echo SECRET_KEY=$SECRET_KEY >> .env.prod
+echo SQL_DATABASE=$SQL_DATABASE >> .env.prod
+echo SQL_USER=$SQL_USER >> .env.prod
+echo SQL_PASSWORD=$SQL_PASSWORD >> .env.prod
+echo SQL_HOST=$SQL_HOST >> .env.prod
+echo SQL_PORT=$SQL_PORT >> .env.prod
 
-echo AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID >> .env
-echo AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY >> .env
-echo AWS_STORAGE_BUCKET_NAME=$AWS_STORAGE_BUCKET_NAME >> .env
+echo AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID >> .env.prod
+echo AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY >> .env.prod
+echo AWS_STORAGE_BUCKET_NAME=$AWS_STORAGE_BUCKET_NAME >> .env.prod
