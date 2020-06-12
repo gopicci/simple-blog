a1d6597019b267d183768437b1bf66315479037d
gopicci <56777940+gopicci@users.noreply.github.com>
2020-06-12 15:57:05 -0700

    CI/CD

--- setup_env.sh
+++ setup_env.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 echo DEBUG=0 >> .env
+echo DJANGO_ALLOWED_HOSTS=$EC2_PUBLIC_IP_ADDRESS >> .env
 echo SQL_ENGINE=django.db.backends.postgresql >> .env
 echo DATABASE=postgres >> .env
 
@@ -14,3 +15,10 @@ echo SQL_PORT=$SQL_PORT >> .env
 echo AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID >> .env
 echo AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY >> .env
 echo AWS_STORAGE_BUCKET_NAME=$AWS_STORAGE_BUCKET_NAME >> .env
+
+echo API_IMAGE=$IMAGE:api  >> .env
+echo NGINX_IMAGE=$IMAGE:nginx  >> .env
+echo CI_REGISTRY_USER=$CI_REGISTRY_USER   >> .env
+echo CI_JOB_TOKEN=$CI_JOB_TOKEN  >> .env
+echo CI_REGISTRY=$CI_REGISTRY  >> .env
+echo IMAGE=$CI_REGISTRY/$CI_PROJECT_NAMESPACE/$CI_PROJECT_NAME >> .env
